719a14c0e6877c23fe34df7e8ca697593986c818
tiko.liu <tiko.liu@cloudwise.com>
2021-12-21 22:07:24 +0800

    init

---
+++ service-provider/src/main/java/org/example/ProviderApplication.java
@@ -0,0 +1,31 @@
+package org.example;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+@RestController
+@RefreshScope // 不加这个不会动态刷新
+public class ProviderApplication {
+
+    @Value("${name}")
+    private String name;
+    @Value("${common}")
+    private String common;
+
+    @RequestMapping("/getName")
+    public String getName() {
+        return name + "---" + common;
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProviderApplication.class, args);
+    }
+
+}
